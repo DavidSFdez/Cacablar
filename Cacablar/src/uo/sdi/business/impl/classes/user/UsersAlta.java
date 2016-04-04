d5d3b738363e5521436f64bc4a00c99904233de8
DavidSFdez <davidsfdez@gmail.com>
2016-04-04 18:39:19 +0200

    -commit chorras
    pasé a negocio una cosa del registrar usuario que se hacía en
    presentacion

--- Cacablar/src/uo/sdi/business/impl/classes/user/UsersAlta.java
+++ Cacablar/src/uo/sdi/business/impl/classes/user/UsersAlta.java
@@ -3,6 +3,7 @@ package uo.sdi.business.impl.classes.user;
 import uo.sdi.business.exception.EntityAlreadyExistsException;
 import uo.sdi.infrastructure.Factories;
 import uo.sdi.model.User;
+import uo.sdi.model.UserStatus;
 import uo.sdi.persistence.UserDao;
 import uo.sdi.persistence.exception.AlreadyPersistedException;
 
@@ -13,6 +14,7 @@ public class UsersAlta {
 	UserDao dao = Factories.persistence.createUserDao();
 
 	try {
+	    user.setStatus(UserStatus.ACTIVE);
 	    dao.save(user);
 	} catch (AlreadyPersistedException e) {
 	    throw new EntityAlreadyExistsException("Usuario ya existe " + user, e);
