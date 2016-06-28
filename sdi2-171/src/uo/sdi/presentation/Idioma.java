0b22ee0cbf53a83ae3df683763ac81a19eede992
J. Sierra <menelcar_0@hotmail.com>
2016-06-29 00:23:56 +0200

    JSF mola que te cagas de la repanocha tío.

---
+++ sdi2-171/src/uo/sdi/presentation/Idioma.java
@@ -0,0 +1,37 @@
+package uo.sdi.presentation;
+
+import java.util.Locale;
+
+public class Idioma {
+    
+    private Locale locale;
+    private String code;
+    private String language;
+
+    public Locale getLocale() {
+	return locale;
+    }
+
+    public void setLocale(Locale locale) {
+	this.locale = locale;
+    }
+
+    public String getLanguage() {
+	return language;
+    }
+
+    public void setLanguage(String language) {
+	this.language = language;
+    }
+
+    public String getCode() {
+	return code;
+    }
+
+    public void setCode(String code) {
+	this.code = code;
+    }
+    
+    
+
+}
--- sdi2-171/src/uo/sdi/presentation/SettingsBean.java
+++ sdi2-171/src/uo/sdi/presentation/SettingsBean.java
@@ -1,7 +1,15 @@
 package uo.sdi.presentation;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
+import java.util.Properties;
 
 import javax.annotation.PreDestroy;
 import javax.faces.bean.ManagedBean;
@@ -14,9 +22,42 @@ import alb.util.log.Log;
 @SessionScoped
 public class SettingsBean implements Serializable {
     private static final long serialVersionUID = 2L;
-    private static final Locale ENGLISH = new Locale("en");
-    private static final Locale SPANISH = new Locale("es");
+//    private static final Locale ENGLISH = new Locale("en");
+//    private static final Locale SPANISH = new Locale("es");
     private Locale locale = new Locale("es");
+    
+    private Map<String, Idioma> mapaIdiomas = new HashMap<String, Idioma>();
+
+    private final static String PATH_TO_PROJECT = "S:\\";
+
+    public SettingsBean() {
+	locale = new Locale("es");
+	File directory = new File(PATH_TO_PROJECT);
+	for (File element : directory.listFiles()) {
+	    if (element.getName().contains("messages")) {
+		process(element);
+	    }
+	}
+    }
+
+    private void process(File element) {
+	String s = element.getName();
+	String name = s.substring(s.indexOf("_") + 1, s.indexOf("."));
+
+	Properties properties = new Properties();
+	try {
+	    properties.load(new FileInputStream(element));
+	} catch (IOException e) {
+	    throw new RuntimeException("No se encuentra el fichero.");
+	}
+
+	Idioma idioma = new Idioma();
+	idioma.setCode(name);
+	idioma.setLocale(new Locale(name));
+	idioma.setLanguage(properties.getProperty("meta.idioma"));
+
+	mapaIdiomas.put(name, idioma);
+    }
 
     @PreDestroy
     public void end() {
@@ -26,17 +67,39 @@ public class SettingsBean implements Serializable {
     public Locale getLocale() {
 	return (locale);
     }
+    
+    
+    public String localeSelected;
 
-    public String setSpanish(ActionEvent event) {
-	Log.info("Cambiando idioma a Español.");
-	locale = SPANISH;
-	return null;
+    public void setLocaleSelected(String localeSelected) {
+	this.localeSelected = localeSelected;
     }
 
-    public String setEnglish(ActionEvent event) {
-	Log.info("Cambiando idioma a Inglés.");
-	locale = ENGLISH;
-	return null;
+    public void changeLanguage(ActionEvent event) {
+	System.out.println(localeSelected);
+	if (mapaIdiomas.containsKey(localeSelected))
+	    locale = mapaIdiomas.get(localeSelected).getLocale();
     }
+
+    public List<Idioma> getIdiomas() {
+	List<Idioma> idiomas = new LinkedList<>();
+	for (Map.Entry<String, Idioma> entry : mapaIdiomas.entrySet())
+	    idiomas.add(entry.getValue());
+	return idiomas;
+    }
+    
+    
+
+//    public String setSpanish(ActionEvent event) {
+//	Log.info("Cambiando idioma a Español.");
+//	locale = SPANISH;
+//	return null;
+//    }
+//
+//    public String setEnglish(ActionEvent event) {
+//	Log.info("Cambiando idioma a Inglés.");
+//	locale = ENGLISH;
+//	return null;
+//    }
 
 }
