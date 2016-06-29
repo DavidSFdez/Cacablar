736432fa1b0cf2373e19db66914f3de6337c95ed
DavidSFdez <davidsfdez@gmail.com>
2016-06-29 12:48:33 +0200

    idiomas intento 2.0.

--- sdi2-171/src/uo/sdi/presentation/SettingsBean.java
+++ sdi2-171/src/uo/sdi/presentation/SettingsBean.java
@@ -11,9 +11,10 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.Properties;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.enterprise.context.SessionScoped;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
 import javax.faces.event.ActionEvent;
 
 import alb.util.log.Log;
@@ -22,41 +23,47 @@ import alb.util.log.Log;
 @SessionScoped
 public class SettingsBean implements Serializable {
     private static final long serialVersionUID = 2L;
-//    private static final Locale ENGLISH = new Locale("en");
-//    private static final Locale SPANISH = new Locale("es");
+    // private static final Locale ENGLISH = new Locale("en");
+    // private static final Locale SPANISH = new Locale("es");
     private Locale locale = new Locale("es");
-    
+
     private Map<String, Idioma> mapaIdiomas = new HashMap<String, Idioma>();
+
+    List<Idioma> idiomas;
 
     private final static String PATH_TO_PROJECT = "S:\\";
 
     public SettingsBean() {
+
+    }
+
+    @PostConstruct
+    private void process() {
 	locale = new Locale("es");
 	File directory = new File(PATH_TO_PROJECT);
 	for (File element : directory.listFiles()) {
 	    if (element.getName().contains("messages")) {
-		process(element);
+
+		String s = element.getName();
+		String name = s.substring(s.indexOf("_") + 1, s.indexOf("."));
+
+		Properties properties = new Properties();
+		try {
+		    properties.load(new FileInputStream(element));
+		} catch (IOException e) {
+		    throw new RuntimeException("No se encuentra el fichero.");
+		}
+
+		Idioma idioma = new Idioma();
+		idioma.setCode(name);
+		System.out.println("name: " + name);
+		idioma.setLocale(new Locale(name));
+		System.out.println("locale: " + idioma.getLocale());
+		idioma.setLanguage(properties.getProperty("meta.idioma"));
+		System.out.println("idioma: " + idioma.getLanguage());
+		mapaIdiomas.put(name, idioma);
 	    }
 	}
-    }
-
-    private void process(File element) {
-	String s = element.getName();
-	String name = s.substring(s.indexOf("_") + 1, s.indexOf("."));
-
-	Properties properties = new Properties();
-	try {
-	    properties.load(new FileInputStream(element));
-	} catch (IOException e) {
-	    throw new RuntimeException("No se encuentra el fichero.");
-	}
-
-	Idioma idioma = new Idioma();
-	idioma.setCode(name);
-	idioma.setLocale(new Locale(name));
-	idioma.setLanguage(properties.getProperty("meta.idioma"));
-
-	mapaIdiomas.put(name, idioma);
     }
 
     @PreDestroy
@@ -67,8 +74,7 @@ public class SettingsBean implements Serializable {
     public Locale getLocale() {
 	return (locale);
     }
-    
-    
+
     public String localeSelected;
 
     public void setLocaleSelected(String localeSelected) {
@@ -82,24 +88,22 @@ public class SettingsBean implements Serializable {
     }
 
     public List<Idioma> getIdiomas() {
-	List<Idioma> idiomas = new LinkedList<>();
+	idiomas = new LinkedList<>();
 	for (Map.Entry<String, Idioma> entry : mapaIdiomas.entrySet())
 	    idiomas.add(entry.getValue());
 	return idiomas;
     }
-    
-    
 
-//    public String setSpanish(ActionEvent event) {
-//	Log.info("Cambiando idioma a Español.");
-//	locale = SPANISH;
-//	return null;
-//    }
-//
-//    public String setEnglish(ActionEvent event) {
-//	Log.info("Cambiando idioma a Inglés.");
-//	locale = ENGLISH;
-//	return null;
-//    }
+    // public String setSpanish(ActionEvent event) {
+    // Log.info("Cambiando idioma a Español.");
+    // locale = SPANISH;
+    // return null;
+    // }
+    //
+    // public String setEnglish(ActionEvent event) {
+    // Log.info("Cambiando idioma a Inglés.");
+    // locale = ENGLISH;
+    // return null;
+    // }
 
 }
